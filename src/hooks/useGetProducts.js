be5352d7a47f8a990fc3c29015747103cc1fbf11
dmira1 <darryl.pmira@gmail.com>
2022-04-05 22:35:54 -0600

    Removed React from import

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,6 +1,6 @@
 import { ref as dataRef, get } from 'firebase/database';
 import {db} from 'libs/firebase';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function useGetProducts(){
     const [productData, setProductData] = useState([]);
